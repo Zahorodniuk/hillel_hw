26e3dd8976ca4494c3ae6fb81ad13cda432c2755
Zahorodniuk <zahorodniuknatalia@gmail.com>
2024-07-16 21:29:47 +0200

    Update hw6.1. Remove unnecessary vars

--- src/hw6/hw6.1.py
+++ src/hw6/hw6.1.py
@@ -1,5 +1,6 @@
 from string import ascii_letters
 
-a, b, c = input('Enter two letters separated by a hyphen: ')
+ascii_range = input('Enter two letters separated by a hyphen: ')
 
-print(ascii_letters[ascii_letters.index(a):ascii_letters.index(c) + 1])
+
+print(ascii_letters[ascii_letters.index(ascii_range[0]):ascii_letters.index(ascii_range[2]) + 1])
