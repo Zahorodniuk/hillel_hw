9ec121ad6c9299dc207b34bdaee4cf6b133f637b
Zahorodniuk <zahorodniuknatalia@gmail.com>
2024-07-17 09:41:12 +0200

    update hw 6.2

--- src/hw6/hw6.2.py
+++ src/hw6/hw6.2.py
@@ -5,23 +5,24 @@ if 0 <= mean < 8640000:
     hours = mean // 3600 - days * 24
     minutes = mean // 60 - days * 1440 - hours * 60
     seconds = mean - days * 86400 - hours * 3600 - minutes * 60
+
+
+    if days > 14:
+        variant = days % 10
+    else:
+        variant = days
+
+    days_mapping = {
+     "1": [2, 3, 4],
+     "2": [1]
+    }
+
+    wrd = 'днів'
+    if variant in days_mapping['1']:
+        wrd = 'дні'
+    elif variant in days_mapping['2']:
+        wrd = 'день'
+
+    print(f'{days} {wrd}, {hours:02}:{minutes:02}:{seconds:02}')
 else:
     print('Try again!')
-
-if days > 14:
-    variant = days % 10
-else:
-    variant = days
-
-days_mapping = {
-    "1": [2, 3, 4],
-    "2": [1]
-}
-
-wrd = 'днів'
-if variant in days_mapping['1']:
-    wrd = 'дні'
-elif variant in days_mapping['2']:
-    wrd = 'день'
-
-print(f'{days} {wrd}, {hours:02}:{minutes:02}:{seconds:02}')
