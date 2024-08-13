47bd6069596ef0ab6d575df78e7c611bfa25955a
Zahorodniuk <zahorodniuknatalia@gmail.com>
2024-08-13 14:03:05 +0200

    hw13

---
+++ src/hw13/hw13.1.py
@@ -0,0 +1,62 @@
+class Human:
+    def __init__(self, gender, age, first_name, last_name):
+        self.gender = gender
+        self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def __str__(self):
+        return f'{self.gender}, {self.age}, {self.first_name}, {self.last_name}'
+
+
+class Student(Human):
+
+    def __init__(self, gender, age, first_name, last_name, record_book):
+        self.gender = gender
+        self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+        self.record_book = record_book
+
+    def __str__(self):
+        return f'{self.gender}, {self.age}, {self.first_name}, {self.last_name}, {self.record_book}'
+
+
+class Group:
+    def __init__(self, number):
+        self.number = number
+        self.students = set()
+
+    def add_student(self, student):
+        self.students.add(student)
+
+    def delete_student(self, last_name):
+        student = self.find_student(last_name)
+        if student:
+            self.students.remove(student)
+
+    def find_student(self, last_name):
+        for student in self.students:
+            if student.last_name == last_name:
+                return student
+        return None
+
+    def __str__(self):
+        all_students = '\n'.join(str(student) for student in self.students)
+        return f'Number: {self.number}\n{all_students}'
+
+
+st1 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
+st2 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
+gr = Group('PD1')
+gr.add_student(st1)
+gr.add_student(st2)
+print(gr)
+assert str(gr.find_student('Jobs')) == str(st1), 'Test1'
+assert gr.find_student('Jobs2') is None, 'Test2'
+assert isinstance(gr.find_student('Jobs'), Student) is True, 'Метод поиска должен возвращать экземпляр'
+
+gr.delete_student('Taylor')
+print(gr)  # Only one student
+
+gr.delete_student('Taylor')  # No error
